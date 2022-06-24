cf36bd73b71a3f7d971581951ae61cfc521fee2a
Fernando Magela <fernando_cie@hotmail.com>
2022-06-24 17:17:45 -0300

    Removido contato e sobre

--- receitas/views.py
+++ receitas/views.py
@@ -1,14 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):
     return render(request, 'receitas/home.html')
-
-
-def contato(request):
-    return HttpResponse('contato156')
-
-
-def sobre(request):
-    return HttpResponse('sobre-everyone')
